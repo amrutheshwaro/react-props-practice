1628e0ae4a6578d14356c077cb63b7fc7f2cad15
Amrutheshwar O <55506973+amrutheshwaro@users.noreply.github.com>
2021-02-26 13:38:25 +0530

    final commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+var avatar = function (props) {
+  return <img className="circle-img" src={props.imgURL} alt="avatar_img" />;
+};
+
+export default avatar;
---
+++ src/components/Detail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+var detail = function (props) {
+  return <p className="info">{props.info}</p>;
+};
+
+export default detail;
